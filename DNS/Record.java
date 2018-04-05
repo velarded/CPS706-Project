38712bb7dd97af143dde5ee527c07fc26824b318
Alpha Mai <alpha.mai@ryerson.ca>
2018-04-05 12:17:02 -0400

    add setters and getters

--- DNS/Record.java
+++ DNS/Record.java
@@ -19,6 +19,36 @@ public class Record {
         
     }
 
+    public void setName(Name name)
+	{
+		this.name = name;
+	}
+	
+	public Name getName()
+	{
+		return name;
+	}
+	
+	public void setValue(String value)
+	{
+		this.value = value;
+	}
+	
+	public String getValue()
+	{
+		return value;
+	}
+	
+	public int getType()
+	{
+		return type;
+	}
+	
+	public void setType(int type)
+	{
+		this.type = type;
+	}
+
     public String toString(){
         String str = "(" + name.toString();
         if(value != null){
