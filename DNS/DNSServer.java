85c81f58ca4c86e2113fde2a97519a2c47ed716a
Alpha Mai <alpha.mai@ryerson.ca>
2018-04-09 23:46:44 -0400

    Add lookup functionality. Currently works locally

--- DNS/DNSServer.java
+++ DNS/DNSServer.java
@@ -3,13 +3,13 @@ import java.io.*;
 import java.net.*;
 import java.util.List;
 
+import DNS.DNSMessage;
 import DNS.Record;
-import sun.tools.jar.resources.jar_de;
 
 import java.util.ArrayList;
 
 public class DNSServer{
-    private List recordTable;
+    private List<Record> recordTable;
     private int port;
     public DNSServer(int port){
         recordTable = new ArrayList<Record>();
@@ -34,26 +34,80 @@ public class DNSServer{
             DNSMessage message = new DNSMessage(receivePacket.getData());
             System.out.println(message.toString());
 
-            DNSMessage response = lookup(message.getQuestion());
+            DNSMessage response = lookup(message);
+            
+            System.out.println(response.toString());
         }
     }
 
-    public DNSMessage lookup(Record question){
-        DNSMessage resp = new DNSMessage();
+    public DNSMessage lookup(DNSMessage message){
+
+        DNSMessage resp = new DNSMessage(message);
+        Record question = message.getQuestion();
         Name qname = question.getName();
+
+        System.out.println("TYPE: " + question.getType().toString());
+
+        //loop through the name label by label
+        //(Ex. na.abc.example.com -> abc.example.com -> example.com -> etc...)
         for(int i = 0; i < qname.labels()-1; i++){
+            //if i is 0 that means we have not remove a label yet
             boolean isExact = (i == 0);
-            
+
             for(int j = 0; j < recordTable.size(); j++){
-                Record record = recordTable.get(i);
+                Record record = recordTable.get(j);
+                System.out.println("Comparing... " + qname.toString() + " " + record.getName().toString());
                 if(qname.equals(record.getName())){
-                    if(isExact && record.getType() == question.getType()){
-                        
+                    if(isExact){
+                        Type rtype = record.getType();
+                        if(rtype == question.getType()){
+                            //if the name and type match with the record we found an answer
+                            resp.addRecord(record, 1);
+                        }
+                        else if(rtype == Type.CN || rtype == Type.R){
+                            //if it is type CN or R then restart the search with new name
+                            resp.addRecord(record, 1);
+                            qname = record.getName();
+                            i = 0;
+                            break;
+                        }
+                    }
+                    if(record.getType() == Type.NS && question.getType() != Type.NS){
+                        //store any NS records if found
+                        resp.addRecord(record, 2);
+                    }
+                }
+            }
+            //remove the first label from name
+            qname = qname.offset(1);
+        }
+
+        //if there are auth records, search for their ip addresses
+        if(resp.getCount(2) > 0){
+            //get all authoritative nameserver records
+            List<Record> NSRecords = resp.getAuthorities();
+            
+            //search through the record table
+            for(int i = 0; i < NSRecords.size(); i++){
+                //convert String to name object
+                Name tname = new Name(NSRecords.get(i).getValue());
+                System.out.println("Looking for " + tname.toString());
+
+                for(int j = 0; j < recordTable.size(); j++){
+                    //search for the type A record for nameserver
+                    Record record = recordTable.get(j);
+
+                    System.out.println("Comparing... " + tname.toString() + " " + record.getName().toString());
+
+                    if(tname.equals(record.getName()) && record.getType() == Type.A){
+                        resp.addRecord(record, 3);
+                        break;
                     }
                 }
             }
         }
         
+        return resp;
     }
 
     public void addRecord(Record record){
