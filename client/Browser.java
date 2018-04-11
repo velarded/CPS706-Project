8ead18953962a157a8bc87a197aa67ceb1ef06e2
Alpha Mai <alpha.mai@ryerson.ca>
2018-04-10 23:11:44 -0400

    Recreate browser classes with better structure

---
+++ client/Browser.java
@@ -0,0 +1,30 @@
+package client;
+import javax.swing.JFrame;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JEditorPane;
+import javax.swing.JTextField;
+
+public class Browser extends JFrame{
+    private JTextField urlField;
+    private JEditorPane editorPane;
+
+    public Browser(){
+
+        urlField = new JTextField("HELLO WORLD", 35);
+        editorPane = new JEditorPane();
+        editorPane.setContentType("text/html");
+        editorPane.setEditable(false);
+
+        getContentPane().setLayout(new BorderLayout());
+        add(urlField, BorderLayout.NORTH);
+        add(editorPane, BorderLayout.CENTER);
+    }
+
+    /*load html to editor pane*/
+    public void load(String html){
+        editorPane.setText(html);
+    }
+
+}
