74128c9149b804819e34cb41c1415827bf1e7618
Alpha Mai <alpha.mai@ryerson.ca>
2018-04-10 22:55:53 -0400

    Edte record table

--- LocalDNS.java
+++ LocalDNS.java
@@ -8,10 +8,10 @@ public class LocalDNS{
 
     public static void main(String[] args) throws Exception {
         DNSServer server = new DNSServer(53);
-        server.addRecord(new Record(new Name("herCDN.com"), "NSherCDN.com", Record.Type.NS));
-        server.addRecord(new Record(new Name("NSherCDN.com"), "SOME IP", Record.Type.A));
-        server.addRecord(new Record(new Name("hiscinema.com"), "NShiscinema.com", Record.Type.NS));
-        server.addRecord(new Record(new Name("NShiscinema.com"), "SOME IP", Record.Type.A));
+        server.addRecord(new Record(new Name("herCDN.com"), "NSherCDN.com", Type.NS));
+        server.addRecord(new Record(new Name("NSherCDN.com"), "SOME IP", Type.A));
+        server.addRecord(new Record(new Name("hiscinema.com"), "NShiscinema.com", Type.NS));
+        server.addRecord(new Record(new Name("NShiscinema.com"), "SOME IP", Type.A));
 
         server.start();
     }
