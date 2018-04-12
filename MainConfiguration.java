5ca9c77a2c2b63096cd363d11566c0b567d2398a
dvelarde <danica_velarde@partech.com>
2018-04-11 23:51:03 -0400

    cleaned up commented code

--- MainConfiguration.java
+++ MainConfiguration.java
@@ -5,27 +5,9 @@ import java.util.Properties;
 public class MainConfiguration 
 {
 	public final static String CONFIG_FILE_LOCATION = "resources/config.txt";
-//	public static int serverTcpPort;
-//	public static String hisCinemaIP;
-//	public static int udpPort;
 	
 	public MainConfiguration()
-	{
-//		File configFile = new File(CONFIG_FILE_LOCATION);
-//		Properties properties = new Properties();
-//		
-//		try
-//		{
-//			properties.load(new FileInputStream(configFile));
-//			serverTcpPort = Integer.parseInt(properties.getProperty("ServerTcpPort"));
-//			hisCinemaIP = properties.getProperty("HisCinemaIP");
-//			udpPort = Integer.parseInt(properties.getProperty("UdpPort"));
-//		}
-//		catch(Exception e)
-//		{
-//			e.printStackTrace();
-//		}
-	}
+	{}
 	
 	public static int hisCinemaServerPort() 
 	{
