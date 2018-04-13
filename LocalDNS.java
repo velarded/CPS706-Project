e7c1bd5cd19b386bdc8feecf1005f171cd399f25
Alpha Mai <alpha.mai@ryerson.ca>
2018-04-13 14:14:17 -0400

    DNS servers now bound to specific ip address and create auth dns

--- LocalDNS.java
+++ LocalDNS.java
@@ -1,17 +1,15 @@
 import DNS.*;
-import java.io.*;
 import java.net.*;
-import java.util.List;
-import java.util.ArrayList;
 
 public class LocalDNS{
 
     public static void main(String[] args) throws Exception {
-        DNSServer server = new DNSServer(53);
+        InetAddress ip = InetAddress.getByName("127.0.0.1");
+        DNSServer server = new DNSServer(ip, 53, false);
         server.addRecord(new Record(new Name("herCDN.com"), "NSherCDN.com", Type.NS));
-        server.addRecord(new Record(new Name("NSherCDN.com"), "SOME IP", Type.A));
+        server.addRecord(new Record(new Name("NSherCDN.com"), "127.0.0.3", Type.A));
         server.addRecord(new Record(new Name("hiscinema.com"), "NShiscinema.com", Type.NS));
-        server.addRecord(new Record(new Name("NShiscinema.com"), "SOME IP", Type.A));
+        server.addRecord(new Record(new Name("NShiscinema.com"), "127.0.0.2", Type.A));
 
         server.start();
     }
